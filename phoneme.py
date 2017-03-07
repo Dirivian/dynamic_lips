d091f89d434bb0282ae664304848bee5e3ec928c
Dirivian <jithindgeorge93@gmail.com>
2017-03-06 20:27:41 -0800

    Extracted Phonemes

--- phoneme.py
+++ phoneme.py
@@ -8,9 +8,11 @@ Created on Sun Mar  5 17:14:51 2017
 import nltk
 
 arpabet = nltk.corpus.cmudict.dict()
-
-for word in ('f', 'see', 'sea', 'compute', 'comput', 'cat'):
-    try:
-        print(arpabet[word][0])
-    except Exception as e:
-        print(e)
+k =[j for j in 'abcdefghijklmnopqrstuvwxyz']
+t= ['again', 'soon', 'now', 'please','bin', 'lay', 'place','set', 'blue', 'green','red','white' ,'at','by', 'with', 'in', 'zero','one', 'two','three','four','five','six','seven','eight','nine'];
+g = k+t
+ph =[]
+for word in g:
+        w =arpabet[word][0]
+        ph = ph +w
+uniqueph = set(ph)
